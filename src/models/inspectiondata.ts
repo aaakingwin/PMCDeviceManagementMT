b03461cbc7f51ae0c4938fc4ac23d3468b3c61a7
aaakingwin <aaakingwin@vip.qq.com>
2018-09-11 20:37:44 +0800

    巡检单

--- src/models/inspectiondata.ts
+++ src/models/inspectiondata.ts
@@ -1,15 +1,21 @@
 import { SysConfig } from "../providers/sysconfig";
-
+//巡检单数据
 export class InspectionData
 {
     Id:string;
-    InspectionDate:string; 
-    Microdistrict:string;   
-    Inspector:string;  
-    AssetNumber:string;
-    AssetName:string;
-    AssetStatus:string;
-    Description:string;  
+    InspectionDate:string; //巡检日期
+    Microdistrict:string;   //所属小区
+    Inspector:string;  //巡检人员
+    AssetNumber:string;//物品代码
+    AssetName:string;//物品名称
+    AssetStatus:string;//物品状态
+    Description:string;  //情况描述
+    Department:string;	//部门科室
+    Township:string;	//所属区镇
+    Community:string;	//所属社区
+    AssetCategory:string;	//物品总类
+    AssetType:string;	//物品类型
+    AssetGroup:string;	//物品分组	
 }
 
 export class InspectionResponse
